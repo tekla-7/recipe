16e05ec3b2b5862afdb61b52409f929f0a1bc23c
tekla kvekvetsia <kvekveciatekla@gmail.com>
2024-04-23 12:10:58 +0400

    pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,46 +1,57 @@
-// import icons from 'url:../img/icons.svg';
-import 'core-js/stable';
-import 'regenerator-runtime/runtime'
-import * as model from './model.js'
-import recipeView from './views/recipeView.js'
+import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
+import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
-import ResultsViewView from './views/resultsView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
-if(module.hot){
-module.hot.accept()
-}
-
-
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
 
 const controlRecipes = async function () {
   try {
-    const id = window.location.hash.slice(1)
-    if (!id) return
-    recipeView.renderSpinner()
-    //loading recipe
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    resultsView.update(model.getSearchResultsPage());
+
+    bookmarksView.update(model.state.bookmarks);
     await model.loadRecipe(id);
-    // const { recipe } = model.state;
-
-    //view Rendering
-    recipeView.render(model.state.recipe)
-
-   
+    recipeView.render(model.state.recipe);
   } catch (err) {
-    recipeView.renderError()
+    recipeView.renderError();
+    console.error(err);
   }
-}
-const controlSearchResuls=async function(){
-  
-  try{
-    ResultsViewView.renderSpinner()
-    const query=searchView.getQuery();
-    if(!query) return ;
-    await model.loadSearchResults('');
-    ResultsViewView.render(model.state.search.results)
+};
 
-  }catch(err){}
-}
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    const query = searchView.getQuery();
+    if (!query) return;
 
+    await model.loadSearchResults(query);
+    resultsView.render(model.getSearchResultsPage());
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  model.updateServings(newServings);
+  recipeView.update(model.state.recipe);
+  paginationView.addHandlerClick(controlPagination)
+};
 
 const init=function(){
 recipeView.addHandlerRender(controlRecipes)
--- src/js/model.js
+++ src/js/model.js
@@ -4,41 +4,85 @@ import { getJSON } from './helper.js';
 import recipeView from './views/recipeView.js';
 export const state = {
     recipe: {},
-    search:{
-        query:'',
-        results:[]
-    }
-}
-export const loadRecipe = async function (id) {
-    try{
-    const data=await getJSON(`API_URL/${id}`)
+    search: {
+      query: '',
+      results: [],
+      page: 1,
+      resultsPerPage: RES_PER_PAGE,
+    },
+    bookmarks: [],
+  };
   
+  const createRecipeObject = function (data) {
     const { recipe } = data.data;
-    state.recipe = {
-        id: recipe.id,
-        title: recipe.title,
-        publisher: recipe.publisher,
-        sourceUrl: recipe.source_url,
-        image: recipe.image_url,
-        servings: recipe.servings,
-        cookingTime: recipe.cooking_time,
-        ingredients: recipe.ingredients
+    return {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+      ...(recipe.key && { key: recipe.key }),
+    };
+  };
+  
+  export const loadRecipe = async function (id) {
+    try {
+      const data = await AJAX(`${API_URL}${id}?key=${KEY}`);
+      state.recipe = createRecipeObject(data);
+  
+      if (state.bookmarks.some(bookmark => bookmark.id === id))
+        state.recipe.bookmarked = true;
+      else state.recipe.bookmarked = false;
+  
+      console.log(state.recipe);
+    } catch (err) {
+      // Temp error handling
+      console.error(`${err} 💥💥💥💥`);
+      throw err;
     }
-    }catch(err){
-       throw err ;
+  };
+  
+  export const loadSearchResults = async function (query) {
+    try {
+      state.search.query = query;
+  
+      const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
+      console.log(data);
+  
+      state.search.results = data.data.recipes.map(rec => {
+        return {
+          id: rec.id,
+          title: rec.title,
+          publisher: rec.publisher,
+          image: rec.image_url,
+          ...(rec.key && { key: rec.key }),
+        };
+      });
+      state.search.page = 1;
+    } catch (err) {
+      console.error(`${err} 💥💥💥💥`);
+      throw err;
     }
-}
-
-export const loadSearchResults=async function(query){
-    try{
-        state.search.query=query
-        const data=await getJSON(`${API_URL}?search=${query}`)
-       state.search.results= data.data.recipe.map(rec=>{
-           return {id: rec.id,
-            title: rec.title,
-            publisher: rec.publisher,
-            image: rec.image_url,}
-          
-        })
-    }catch(err){throw err}
-}
+  };
+  
+  export const getSearchResultsPage = function (page = state.search.page) {
+    state.search.page = page;
+  
+    const start = (page - 1) * state.search.resultsPerPage; // 0
+    const end = page * state.search.resultsPerPage; // 9
+  
+    return state.search.results.slice(start, end);
+  };
+  
+  export const updateServings = function (newServings) {
+    state.recipe.ingredients.forEach(ing => {
+      ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+      // newQt = oldQt * newServings / oldServings // 2 * 8 / 4 = 4
+    });
+  
+    state.recipe.servings = newServings;
+  };
+  
